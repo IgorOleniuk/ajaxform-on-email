6f831f73cdb5333434a27a44a1e55752145a5c4b
Igor Oleniuk <igor.oleniuk1@gmail.com>
2019-09-03 16:16:36 +0300

    new time

--- formMail.js
+++ formMail.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
-    $('#form button').on('click', function () {
-
-               var formData = $(this).serialize(); //собераем все данные из формы
-               $.ajax({
-               type: 'POST', //Метод отправки
-               url: 'handler.php', //путь до php фаила отправителя
-               data: formData,
-                       success: function(data){ // сoбытиe пoслe удaчнoгo oбрaщeния к сeрвeру и пoлучeния oтвeтa
-                       alert('все ок'); // пoкaжeм eё тeкст
-                       }
-               });
-       });
-   });
+    $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
+            var form_data = $(this).serialize(); //собераем все данные из формы
+            $.ajax({
+            type: "POST", //Метод отправки
+            url: "send.php", //путь до php фаила отправителя
+            data: form_data,
+            success: function() {
+                   //код в этом блоке выполняется при успешной отправке сообщения
+                   alert("Ваше сообщение отпрвлено!");
+            });
+    });
+});
--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")&&(isset($_POST['message'])&&$_POST['message']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
-        $to = 'ivanigorolenyk@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'ivanigorolemyk@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
         $subject = 'Обратный звонок'; //Загаловок сообщения
         $message = '
                 <html>
@@ -8,7 +8,6 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST[
                         <title>'.$subject.'</title>
                     </head>
                     <body>
-                        <p>Сообщения: '$_POST['message'].'</p>
                         <p>Имя: '.$_POST['name'].'</p>
                         <p>Телефон: '.$_POST['phone'].'</p>
                     </body>
@@ -16,4 +15,5 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST[
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
         $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
- ?>
+}
+?>
