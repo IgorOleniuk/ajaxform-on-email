0f1a33caa3ed4e8659d60d721e343701685260cb
Igor Oleniuk <igor.oleniuk1@gmail.com>
2019-09-03 15:50:27 +0300

    fourth commit

--- handler.php
+++ handler.php
@@ -1,7 +1,6 @@
-<?php
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")
-    &&(isset($_POST['message'])&&$_POST['message']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
-        $to = 'ivanigorolenyl@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")&&(isset($_POST['message'])&&$_POST['message']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'ivanigorolenyk@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
         $subject = 'Обратный звонок'; //Загаловок сообщения
         $message = '
                 <html>
@@ -9,12 +8,12 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST[
                         <title>'.$subject.'</title>
                     </head>
                     <body>
+                        <p>Сообщения: '$_POST['message'].'</p>
                         <p>Имя: '.$_POST['name'].'</p>
                         <p>Телефон: '.$_POST['phone'].'</p>
                     </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-          //Наименование и почта отправителя
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
-}
-?>
+ ?>
